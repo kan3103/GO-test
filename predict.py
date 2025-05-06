0e8d65340499dd60245e1ca84c2611d5891616d3
kan3103 <khang310304@gmail.com>
2025-05-06 23:33:25 +0700

    add requirements

--- predict.py
+++ predict.py
@@ -15,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # use gpu 
 from torchvision import models
 
 
-model = models.resnet50(pretrained=True)
+model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
 
 # If you want to do finetuning then set requires_grad = False
 # Remove these two lines if you want to train entire model,
